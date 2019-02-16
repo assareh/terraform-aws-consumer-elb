ceef925c50570113fe8decfed4ff67df336a63cf
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-02-15 22:51:29 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
 variable "name" {
   description = "The name of the ELB"
 }
+variable "instances" {
+  description = "The name of the ELB"
+  type = list
+}
+variable "instance_count" {
+  description = "The name of the ELB"
+  default = 2
+}
