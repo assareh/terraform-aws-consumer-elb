9d03c9fb8874037dd65ced30d8c163c1222950bc
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-02-18 15:36:40 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "name" {
-  description = "The name of the ELB"
+  description = "The name of the ELB or app.  example: Consumer-App"
 }
 variable "instances" {
-  description = "The name of the ELB"
+  description = "A list containing ec2 instances for the load balancer to use.  examples: ["${module.ec2_instance.id}"] or ["i-1234567890abcdef0","i-2345678901abcdef2"]"
   type = "list"
 }
 variable "number_of_instances" {
-  description = "The name of the ELB"
+  description = "The number of EC2 instances to deploy behind the load balancer.  default: 2"
   default = 2
 }
