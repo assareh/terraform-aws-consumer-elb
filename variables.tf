a9c14940ff45afcb961c4150cfe561c9583a9451
Austin Workman <austin@hashicorp.com>
2019-07-18 11:02:25 -0500

    Updating module for terraform 0.12

--- variables.tf
+++ variables.tf
@@ -1,16 +1,18 @@
 variable "name" {
   description = "The name of the ELB or app.  example: Consumer-App"
 }
+
 variable "instances" {
   description = "A list containing ec2 instances for the load balancer to use.  Can be interpolated from ec2_instance module"
-  type = "list"
+  type        = list(string)
 }
+
 variable "number_of_instances" {
   description = "The number of EC2 instances to deploy behind the load balancer.  default: 2"
-  default = 2
+  default     = 2
 }
 
 variable "environment" {
-  description = "The environment this will be deployed into.  examples: dev, stage, prod
-| [more info:](https://github.com/mocofound/terraform-aws-consumer-elb/edit/master/readme.md)"
+  description = "The environment this will be deployed into.  examples: dev, stage, prod | [more info:](https://github.com/mocofound/terraform-aws-consumer-elb/edit/master/readme.md)"
 }
+
