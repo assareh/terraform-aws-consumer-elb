246bd2759e4f19b6ca3d2c5eac09281503bb14cb
Andy Assareh <assareh@hashicorp.com>
2022-01-14 16:05:29 -0800

    changed to port 80

--- main.tf
+++ main.tf
@@ -10,12 +10,12 @@ resource "aws_elb" "example" {
     unhealthy_threshold = 2
     timeout             = 3
     interval            = 30
-    target              = "HTTP:8080/"
+    target              = "HTTP:80/"
   }
   listener {
     lb_port           = 80
     lb_protocol       = "http"
-    instance_port     = "8080"
+    instance_port     = "80"
     instance_protocol = "http"
   }
 }
