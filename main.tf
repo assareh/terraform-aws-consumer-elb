cf0b6e5c096a9fbe99778e8b3b697b6d59be84cd
assareh <assareh@me.com>
2022-01-14 16:44:52 -0800

    test - removing health check

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ resource "aws_elb" "example" {
   name               = "${var.name}-${var.environment}"
   security_groups    = [aws_security_group.elb.id]
   availability_zones = data.aws_availability_zones.available.names
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    interval            = 30
-    target              = "HTTP:80/"
-  }
+  // health_check {
+  //   healthy_threshold   = 2
+  //   unhealthy_threshold = 2
+  //   timeout             = 3
+  //   interval            = 30
+  //   target              = "HTTP:80/"
+  // }
   listener {
     lb_port           = 80
     lb_protocol       = "http"
